c3fb5f269c5c3e660b5d8bcfd940d61a692a5fa7
Maxson de Santana Carvalho <maxson.carvalho@findes.org.br>
2022-04-04 23:59:11 -0300

    Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -6,17 +6,43 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './error-404/error-404.component';
+import { DataBindingComponent } from './dataBinding/dataBinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([//Espera um array do tipo objetos de rotas
+      {
+        path: '',//default path
+        redirectTo: 'courses',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dataBinding',
+        component: DataBindingComponent
+      },
+      {
+        path: 'courses',
+        component: CourseListComponent
+      },
+      {
+        path: '**',//catch all path
+        component: Error404Component        
+      }      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dataBinding/dataBinding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: "./dataBinding.component.html",
+})
+
+export class DataBindingComponent {
+    templateUrl: './dataBinding.component.html';
+    title = 'course-manager';
+    name: string = 'Maxson';
+}
